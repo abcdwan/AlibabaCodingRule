2393c1db6efbefaeb3fac49152446a2ee47d9f93
邓志强 <dengzhiqiang@konka.com>
2022-08-15 09:25:03 +0800

    Gitee Commit

--- src/codingrule/listhandler/ForEachOrIterator.java
+++ src/codingrule/listhandler/ForEachOrIterator.java
@@ -32,6 +32,7 @@ public class ForEachOrIterator {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("Gitee Test");
     }
 }
 
